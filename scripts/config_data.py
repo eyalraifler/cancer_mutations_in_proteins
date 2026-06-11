c9a1e412a5da6fbe8cb406dcba099263f2bed7c8
eyalraifler <eyalraifler3@gmail.com>
2026-06-11 16:17:00 +0300

    change the document

--- scripts/config_data.py
+++ scripts/config_data.py
@@ -1,15 +1,31 @@
-KRAS_DOMAINS = {
+#for running the regression with different domain definitions, 
+# we can switch between these dictionaries in main.py
+
+#זווית 1
+KRAS_DOMAINS_BY_DEFENITION = {
+    (1, 166): "G Domain",
+}
+
+#זווית 2
+KRAS_ALL_REGIONS_WITH_FUNCTION = {
     (10, 14): "P-loop",
     (30, 40): "Switch I",
     (58, 72): "Switch II",
     (185, 188): "CAAX motif"
 }
 
-#for running the regression with different domain definitions, 
-# we can switch between these dictionaries in main.py
-
 #זווית 1
-EGFR_ALL_DOMAINS = {
+EGFR_DOMAINS_BY_DEFENITION = {
+    (25, 189):  "Domain I (Ligand binding)",
+    (190, 333): "Domain II (Dimerization interface)",
+    (334, 504): "Domain III (Ligand binding)",
+    (505, 645): "Domain IV (CR2)",
+    (713, 973): "Tyrosine Kinase Domain",
+}
+
+
+#זווית 2
+EGFR_ALL_REGIONS_WITH_FUNCTION = {
     (25, 189): "Domain I (L1)",
     (190, 333): "Domain II (CR1)",
     (334, 504): "Domain III (L2)",
@@ -22,19 +38,6 @@ EGFR_ALL_DOMAINS = {
     (974, 1210): "C-terminal Tail"
 }
 
-#זווית 2
-EGFR_DOMAINS_BY_THE_ARTICLE = {
-    (25, 189):    "Domain I (Ligand binding)",
-    (190, 333):   "Domain II (Dimerization interface)",
-    (334, 504):   "Domain III (Ligand binding)",
-    (505, 645): "Domain IV (CR2)",
-    (713, 973): "Tyrosine Kinase Domain",
-}
-
-#זווית 3
-EGFR_DOMAINS_BY_UNIPROT = {
-    (712, 979): "Kinase Domain",
-}
 
 #רק האזורים המשמעותיים בכל דומיין
 EGFR_DOMAINS_INPORTANT_ONES = {
